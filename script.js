b78828b94a9332876e729c6a3d7d0fcd5f74bfb0
StephBelAir <82537281+StephBelAir@users.noreply.github.com>
2022-09-06 11:48:46 +0200

    add bubble sort

--- script.js
+++ script.js
@@ -22,3 +22,42 @@ fetch(urlNumbersApi)
     .catch((err)=>{
         console.log(err);
     })
+
+/*------ Todo - Dynamiser : Utiliser numbers de l'api au lieu de arr
+              - afficher dans l'html
+              - bouton ask sort https://codepen.io/fabienhenon/pen/RwovXJd
+------*/
+
+
+//optimized bubble sort
+
+const arr = [ 4, 6, 3, 9, 9, 3, 9, 6, 9, 1 ];
+console.log(arr);
+
+
+let optimizedBubbleSort = (arr) => {
+    let n = arr.length;
+    let swapped = false;
+
+    for(let i = 0; i < n - 1; i++){
+        for(let j = 0; j <= n - i - 1; j++){
+            if(arr[j] > arr[j+1]){
+                [arr[j], arr[j+1]] = [arr[j+1], arr[j]];
+                swapped = true;
+            }
+        }
+        if(!swapped){
+            break;
+        }
+    }
+    return arr;
+}
+
+optimizedBubbleSort(arr);
+console.log(arr);
+
+
+/*------ Sources Algo ------*/
+// https://askcodez.com/algorithme-de-tri-bubble-javascript.html
+// https://learnersbucket.com/examples/algorithms/bubble-sort-algorithm-in-javascript/
+
